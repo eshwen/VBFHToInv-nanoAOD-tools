4c82949642b613b5cd2d6390c165aae254efd76e
Eshwen <eshwen.bhal@bristol.ac.uk>
2018-11-21 18:46:06 +0100

    Added file lists for privately produced samples. In python form so they can be imported into PSet.py simply

---
+++ crab/file_lists_priv_prod/__init__.py
@@ -0,0 +1,4 @@
+#from file_lists_priv_prod.WplusH_v1 import WplusH_file_list
+#from file_lists_priv_prod.WminusH_v1 import WminusH_file_list
+#from file_lists_priv_prod.ZH_v1 import ZH_file_list
+#from file_lists_priv_prod.ttH_v1 import ttH_file_list
