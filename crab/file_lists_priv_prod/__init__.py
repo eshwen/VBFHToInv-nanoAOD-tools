30a7feab611fede964fcfcfe1374d3bb209f44a3
Eshwen <eshwen.bhal@bristol.ac.uk>
2018-11-22 17:48:16 +0100

    Replaced Python files containing nanoAOD files with a plain text version

--- crab/file_lists_priv_prod/__init__.py
+++
@@ -1,4 +0,0 @@
-#from file_lists_priv_prod.WplusH_v1 import WplusH_file_list
-#from file_lists_priv_prod.WminusH_v1 import WminusH_file_list
-#from file_lists_priv_prod.ZH_v1 import ZH_file_list
-#from file_lists_priv_prod.ttH_v1 import ttH_file_list
